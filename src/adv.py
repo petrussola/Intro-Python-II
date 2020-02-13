1252b262d9a5fba4b42b8dd7f24e592bcf5fc931
Pere <pere.solaclaver@gmail.com>
2020-02-13 15:36:21 +0000

    Build inventory logic

--- src/adv.py
+++ src/adv.py
@@ -35,18 +35,17 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
 player1 = Player("Pere", room["outside"])
 gameOn = True
 directions = ['n', 'e', 's', 'w']
-# Write a loop that:
-#
-# print(player1.current_room.n_to, "<<<")
+
 print(player1.current_room)
+
+
+def missing_input():
+    print(f"I didn't understand that. Can you elaborate?")
+
+
 while gameOn:
     # print items that the player has, for dev purposes
     player_items = ''
@@ -80,13 +79,61 @@ while gameOn:
                     player1.current_room, direction_input)
                 print(player1.current_room)
     elif clean_input[0] == "pick":
-        # print(player1, "<<< player items")
-        # print(clean_input[1], "<<< object being picked")
-        # print(getattr(
-        #     player1.current_room.items["rock"], "description"), "<<< items in player class")
+        # set up boolean variable that we will use to display message in case item is not found
+        found_item = False
+        # iterates over the items in the current room
         for item in player1.current_room.items:
+            # if the element input by the user matches an element in the room
             if item.name == clean_input[1]:
+                # add the item in the player's items list
                 setattr(player1, "items", [*player1.items, item])
+                # delete the item from the current_room's items list
                 player1.current_room.items.remove(item)
+                # set found_item to True
+                found_item = True
+                # print message
+                print(f"Great, you have picked up the {clean_input[1]}!")
+                continue
+        # if found_item is False
+        if not found_item:
+            # print message
+            print(f"There is no {clean_input[1]} in the room")
+    # if user wishes to drop an object
+    elif clean_input[0] == "drop":
+        # if user doesn't have items
+        if len(player1.items) == 0:
+            # display message
+            print("You don't have any items. What about picking up one first? :p")
+        # if user didn't say what to drop
+        elif len(clean_input) == 1:
+            # print message
+            missing_input()
+        else:
+            # we set the variable to false that will help us see if we found the item in player's pocket
+            found_item = False
+            # loop of items owned by player
+            for item in player1.items:
+                # if we find the item in player's pocket
+                if item.name == clean_input[1]:
+                    # return item to the room
+                    setattr(player1.current_room, "items", [
+                            *player1.current_room.items, item])
+                    # remove item from player's pocket
+                    player1.items.remove(item)
+                    # set found item to True
+                    found_item = True
+            # if we didn't find item in player's pocket
+            if not found_item:
+                # print a message
+                print(f"You don't have a {clean_input[1]}")
+    elif clean_input[0] == "i" or clean_input[0] == "inventory":
+        if len(player1.items) == 0:
+            print("You don't have items in your bag")
+        else:
+            output = ''
+            output += f"You have {len(player1.items)} items in your bag: \n"
+            for item in player1.items:
+                output += f"{item.name}\n"
+            print(output)
     else:
         print("Sorry, did not understand that. Do you want to move or pick up an item?")
